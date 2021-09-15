dc47ab9f2828b4517d3586035dbca09e713ee4c1
Evance23 <evancebarracks01@gmail.com>
2021-09-15 06:07:25 +0300

    Add new files

---
+++ manage.py
@@ -0,0 +1,19 @@
+from app import create_app
+from flask_script import Manager, Server
+
+app = create_app('development') #the app instance
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+@manager.command
+def test():
+    '''
+    run the unit test
+    '''
+    import unittest
+    tests= unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+if __name__ =='__main__':
+    manager.run()
