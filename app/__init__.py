53facdf3c91eba5485bad9d1cdac7d9e1d65f23b
Evance23 <evancebarracks01@gmail.com>
2021-09-15 04:44:51 +0300

    kinitialize application

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,19 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from  config import config_options
 
-# Initializing application
-app = Flask(__name__)
+bootstrap = Bootstrap()
 
-from app import views
+def create_app(config_name):
+    app = Flask(__name__,instance_relative_config= True)
+
+    app.config.from_object(config_options[config_name])
+    bootstrap.init_app(app)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .request import config_request
+    config_request(app)
+
+    return app
