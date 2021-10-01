137efef696c7373467eb2a1df9c45677a5a177c4
Evance23 <evancebarracks01@gmail.com>
2021-10-01 08:20:40 +0300

    Add file changes

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from flask_bootstrap import Bootstrap
 bootstrap = Bootstrap()
 
 def create_app(config_name):
-    app = Flask(__name__)
+    app = Flask(__name__) 
 
   # Creating the app configurations
     app.config.from_object(config_options[config_name])
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 export NEWS_API_KEY='f816bfd21a05493dbd34c0c0204282c9'
 export SECRET_KEY='abcde'
 
-python3.6 manage.py server 
+python3 manage.py server 
